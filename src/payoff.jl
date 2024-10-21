f3d843ae96414012d2ae0fab9d892ed264247bbc
Daniel Saurez <79597253+RandalTheVandal03@users.noreply.github.com>
2024-10-21 15:57:19 -0400

    Integration complete (#16)
    * Integration complete
    
    * Removed main
    
    * Added payoffs.jl to our package
    
    Also made some minor updates to models.jl demo.
    
    ---------
    
    Co-authored-by: George <grauta@ufl.edu>

--- src/payoff.jl
+++ src/payoff.jl
@@ -1,33 +1,15 @@
 using Combinatorics
 
+export Province, create_payoffs
+
 # Province struct
 struct Province
     name::String
     money::Int
 end
 
-
-# # Function to generate the power set of provinces
-# function power_set(country::Vector{Province})
-#     pow_list = Vector{Vector{Province}}([])
-#     size = 2 ^ length(country)
-#     push!(pow_list, Vector{Province}())
-
-#     for prov in country
-#         pow_set_size = length(pow_list)
-#         for x in 1:pow_set_size
-#             temp_vec = copy(pow_list[x])
-#             push!(temp_vec, prov)
-#             push!(pow_list, temp_vec)
-#         end
-#     end
-
-#     # Remove the empty set
-#     return pow_list[2:end]
-# end
-
 # Function to print payoffs based on conversion rates
-function print_payoffs(power_set, conversion_rates::Dict{Int,Int})
+function create_payoffs(power_set, conversion_rates::Dict{Int,Int})
     payOffs = Dict()
     for set in power_set
         if isempty(set)
@@ -59,33 +41,7 @@ function print_payoffs(power_set, conversion_rates::Dict{Int,Int})
             end
         end
 
-        # Print the result
-        #println("(" * join(name_list, ", ") * ") can buy: $best_fit_crate crates with a total of \$$val_sum")
         push!(payOffs, name_list => best_fit_crate)
     end
     payOffs
 end
-
-# Main function to run the program
-function main()
-
-    #Establishes Provinces
-    provA = Province("A", 700)
-    provB = Province("B",400)
-    provC = Province("C", 400)
-
-    Country = [provA, provB, provC]
-
-    #Establishes Conversion Rates Dict
-    ConversionRates = Dict{Int64, Int64}(900 => 600, 1500 => 900, 2000 => 1500)
-
-    # Generate the power set of provinces
-    coalitions = powerset(Country)
-    
-
-    # Print the payoffs
-    print_payoffs(coalitions, ConversionRates)
-end
-
-# Call the main function
-main()
