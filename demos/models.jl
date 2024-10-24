10edc8bab9b03adee742d36ee44d876d8bc2dc50
thomascolehewatt <thomashewatt@ufl.edu>
2024-10-24 12:06:00 -0400

    Refactoring to Julia + implementing function in models.jl

--- demos/models.jl
+++ demos/models.jl
@@ -10,14 +10,18 @@ provB = Province("B",400)
 provC = Province("C", 400)
 
 Country = [provA, provB, provC]
+total_budget = 1500.0
 
 #Establishes Conversion Rates Dict
 ConversionRates = Dict{Int64, Int64}(900 => 600, 1500 => 900, 2000 => 1500)
 
+println(calculate_budgets(players, total_budget))
+
 # Generate the power set of provinces
-coalitions = powerset(Country)
+coalitions = powerset(calculate_budgets(players, total_budget))
 
 payoff = create_payoffs(coalitions, ConversionRates)
+println(payoff)
 
 shapley_A = ((2 * 0) + 600 + 600 + (2 * 900)) / 6
 shapley_B = ((2 * 0) + 600 + 0 + (2 * 300)) / 6
--- src/payoff.jl
+++ src/payoff.jl
@@ -1,11 +1,38 @@
 using Combinatorics
 
-export Province, create_payoffs
+export Province, create_payoffs, calculate_budgets
 
 # Province struct
 struct Province
     name::String
-    money::Int
+    money::Float64
+end
+
+# Budget function based on 1/x
+function budget_function(x::Int)
+    return 1.0 / (x + 1)
+end
+
+# Function to calculate budgets and return a vector of Province structs
+function calculate_budgets(players::Vector{String}, total_resources::Float64 = 100.0)
+    num_provinces = length(players)
+    raw_budgets = Vector{Float64}(undef, num_provinces)
+    total_raw_budget = 0.0
+
+    # Calculate budgets based on 1/x and sum them
+    for i in 0:(num_provinces - 1)
+        raw_budgets[i + 1] = budget_function(i)
+        total_raw_budget += raw_budgets[i + 1]
+    end
+
+    country = Vector{Province}(undef, num_provinces)
+
+    for i in 1:num_provinces
+        normalized_budget = (raw_budgets[i] / total_raw_budget) * total_resources
+        country[i] = Province(players[i], normalized_budget) 
+    end
+
+    return country
 end
 
 # Function to print payoffs based on conversion rates
