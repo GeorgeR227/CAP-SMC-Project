f11e4fb4f8032dc68f9443a636c1c779b3616b71
RandalTheVandal03 <surazberry@gmail.com>
2024-12-10 16:48:09 -0500

    Not all files were saved

--- test/mechanism.jl
+++ test/mechanism.jl
@@ -4,7 +4,6 @@ using CAP_SMC_Project
 function test_redistribution(country, budget, tax_type, expected_money) 
   res = sort_by_name(redistribution(country, budget; tax_type = tax_type))
   for (idx, val) in enumerate(expected_money)
-    println(res[idx].name + " " + res[idx].money)
     @test res[idx].name == country[idx].name && res[idx].money == expected_money[idx]
   end
 end
@@ -23,7 +22,7 @@ end
   test_redistribution(country, 0, :prop, [700, 400, 400])
 
   test_redistribution(country, 300, :flat, [600, 450, 450])
-  test_redistribution(country, 300, :prop, [600, 450, 450])
+  test_redistribution(country, 300, :prop, [560, 470, 470])
 
   test_redistribution(country, 3000, :flat, [500, 500, 500])
   test_redistribution(country, 3000, :prop, [500, 500, 500])
